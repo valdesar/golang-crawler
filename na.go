5cccbe0bbefd97b3d781e79fbd8a40a907443df7
Vladimir Sandaevsky <me@sv101.ru>
2019-07-03 21:35:10 +0500

    done with status code analization

--- na.go
+++ na.go
@@ -5,29 +5,70 @@ import(
     "bufio"
     "fmt"
     "log"
-     "io/ioutil"
     "net/http"
-//    "reflect"
+    "strconv"
+    "time"
+    "net"
 )
 
 func getreq(siteNameQ string) {
 
-fmt.Println(siteNameQ)
+siteNameFull := "http://" + siteNameQ
 
-resp, err := http.Get(siteNameQ)
-if err != nil {
-    log.Fatalln(err)
+client := &http.Client{ Timeout: 5 * time.Second,
+    CheckRedirect: func(req *http.Request, via []*http.Request) error {
+      return http.ErrUseLastResponse
+  } }
+
+resp, err := client.Get(siteNameFull)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if resp.StatusCode == 301 {
+	resp, err := http.Get(siteNameFull)
+	    if err != nil {
+    	    log.Fatal(err)
+	    }  
+	    fmt.Println(siteNameFull)
+	    fmt.Println("301 ", resp.StatusCode)
+
 }
 
-defer resp.Body.Close()
 
+    if resp.StatusCode == 302 {
+	resp, err := http.Get(siteNameFull)
+	    if err != nil {
+    	    log.Fatal(err)
+	    }  
+	    fmt.Println(siteNameFull)
+	    fmt.Println("302 ", resp.StatusCode)
 
-body, err := ioutil.ReadAll(resp.Body)
-if err != nil {
-log.Fatalln(err)
 }
 
-log.Println(string(body))
+
+if resp.StatusCode != 301 && resp.StatusCode != 302  {
+
+    // Print the HTTP Status Code and Status Name
+    fmt.Println(siteNameFull)
+    fmt.Println("", resp.StatusCode)
+//, http.StatusText(resp.StatusCode))
+}
+}
+
+
+func askDns(siteNameQ string) {
+
+
+ips, err := net.LookupIP(siteNameQ)
+    if err != nil {
+	fmt.Fprintf(os.Stderr, "Could not get IPs: %v\n", err)
+	os.Exit(1)
+    }
+    for _, ip := range ips {
+    fmt.Printf(" IN A %s\n", ip.String())
+	
+    }
 
 }
 
@@ -41,7 +82,8 @@ func main() {
 
     fmt.Println(pathToFile)
     fmt.Println(numberOfThreads)
-
+    numberInt,_ := strconv.Atoi(numberOfThreads)
+    fmt.Println(numberInt)
 
 
    file, err := os.Open(pathToFile)
@@ -52,14 +94,13 @@ func main() {
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-        siteNameQ := scanner.Text()
-        fmt.Println(scanner.Text())
-        getreq(siteNameQ)
-
-    }
-
+       siteNameQ := scanner.Text()
+//       fmt.Println(scanner.Text())
+       askDns(siteNameQ)
+       go getreq(siteNameQ)
+       time.Sleep(300 * time.Millisecond)
+}
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
-
 }
